99ed627b23585a42e831e8b8cab1dd90eec66830
Che Shian Hung <cheshianhung@Ches-MacBook-Pro.local>
2017-07-12 09:25:59 -0700

    Characters model imported, following bug fixed

--- Assets/Scripts/PlayerFollow.cs
+++ Assets/Scripts/PlayerFollow.cs
@@ -9,22 +9,26 @@ public class PlayerFollow : MonoBehaviour {
 
 	private Queue<Vector3> transformQueue;
 	private Vector3 targetStartPosition;
+	private Vector3 targetPreviousPosition;
 	private Vector3 startPosition;
 	private float positionLerpFrame;
 
 	void Start () {
 		transformQueue = new Queue<Vector3> ();
 		targetStartPosition = playerToFollow.transform.position;
+		targetPreviousPosition = playerToFollow.transform.position;
 		startPosition = transform.position;
 		positionLerpFrame = 0f;
 	}
 	
 	void FixedUpdate () {
 		Follow ();
+		targetPreviousPosition = playerToFollow.transform.position;
 	}
 
 	void OnEnable() {
 		startPosition = transform.position;
+		targetPreviousPosition = playerToFollow.transform.position;
 		positionLerpFrame = 0f;
 	}
 
@@ -43,18 +47,22 @@ public class PlayerFollow : MonoBehaviour {
 	}
 
 	private void PositionUpdate(){
-		if (transformQueue.Count == queueSizeLimit) {
-			Vector3 pastPosition = transformQueue.Dequeue ();
-			transform.position = new Vector3 (pastPosition.x, transform.position.y, pastPosition.z);
-		} else if (transformQueue.Count == 0) {
-			targetStartPosition = playerToFollow.transform.position;
-		}else {
-			startPosition.y = transform.position.y;
-			targetStartPosition.y = transform.position.y;
-			transform.position = Vector3.Lerp (startPosition, targetStartPosition, positionLerpFrame);
-			positionLerpFrame += (float) 1f / queueSizeLimit;
+		Vector3 targetCurrentPosition = playerToFollow.transform.position;
+		print (Mathf.Abs (targetCurrentPosition.x - targetPreviousPosition.x));
+		if (Mathf.Abs (targetCurrentPosition.x - targetPreviousPosition.x) > 0.05f || Mathf.Abs (targetCurrentPosition.z - targetPreviousPosition.z) > 0.05f) {
+			if (transformQueue.Count == queueSizeLimit) {
+				Vector3 pastPosition = transformQueue.Dequeue ();
+				transform.position = new Vector3 (pastPosition.x, transform.position.y, pastPosition.z);
+			} else if (transformQueue.Count == 0) {
+				targetStartPosition = playerToFollow.transform.position;
+			} else {
+				startPosition.y = transform.position.y;
+				targetStartPosition.y = transform.position.y;
+				transform.position = Vector3.Lerp (startPosition, targetStartPosition, positionLerpFrame);
+				positionLerpFrame += (float)1f / queueSizeLimit;
+			}
+			transformQueue.Enqueue (playerToFollow.transform.position);
 		}
-		transformQueue.Enqueue (playerToFollow.transform.position);
 	}
 
 	private void RotationUpdate(){
